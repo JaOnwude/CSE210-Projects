00354539b5ff3bd196ad82c16f19b8ffeb15fa7b
JaOnwude <ojamesuchenna61@gmail.com>
2025-05-08 12:24:12 +0100

    Completed WK01 assignment

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,43 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
-    }
+        
+        // Console.Write("What is the magic number? ");
+        // string magicStringNumber = Console.ReadLine();
+        // int magicNumber = int.Parse(magicStringNumber);
+        Console.Write("What is your guess? ");
+        string userGuess = Console.ReadLine();
+        int userGuessNumber = int.Parse(userGuess);
+
+        Random randomNumberGenerator = new Random();
+        int randomNumber = randomNumberGenerator.Next(1, 101);
+        // Console.WriteLine($"Random Number: {randomNumber}");
+        int numberOfGuesses = 1;
+
+         while (userGuessNumber != randomNumber)
+        {
+            numberOfGuesses++;           
+            if (userGuessNumber > randomNumber)
+            {
+                Console.WriteLine("Lower.");
+                Console.Write("What is your guess? ");
+                userGuess = Console.ReadLine();
+                userGuessNumber = int.Parse(userGuess);
+            }
+            else
+            {
+                Console.WriteLine("Higher.");
+                Console.Write("What is your guess? ");
+                userGuess = Console.ReadLine();
+                userGuessNumber = int.Parse(userGuess);
+            }
+        
+        } 
+                            
+        Console.WriteLine("You guessed it!"); 
+        Console.WriteLine($"You made {numberOfGuesses} guesses.");
+
+    //     Console.Write("Do you want to play again (yes, no)? ");
+    //     string userResponse = Console.ReadLine();
+    // }
 }
