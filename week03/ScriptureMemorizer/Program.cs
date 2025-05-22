4661d90dd9135554f0386dbc3013ad0c2f19d606
JaOnwude <ojamesuchenna61@gmail.com>
2025-05-23 00:23:04 +0100

    Yet to do stretch assignment

--- week03/ScriptureMemorizer/Program.cs
+++ week03/ScriptureMemorizer/Program.cs
@@ -1,9 +1,35 @@
 using System;
+using System.Collections.Generic;
+
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello World! This is the ScriptureMemorizer Project.");
+        Reference reference = new Reference("Jeremiah", 29, 11);
+        Scripture scripture = new Scripture(reference, "For I know the plans I have for you, says the Lord. They are plans for good and not for evil, to give you a future and a hope.");
+
+        while (true)
+        {
+            Console.Clear();
+            Console.WriteLine(scripture.GetDisplayText());
+
+            Console.Write("Press the ENTER key to hide words or type 'Quit' to exit: ");
+            string input = Console.ReadLine();
+
+            if (input.ToLower() == "quit")
+            {
+                break;
+            }
+
+            scripture.HideRandomWords(3);
+
+            if (scripture.AreAllWordsHidden())
+            {
+                Console.Clear();
+                Console.WriteLine(scripture.GetDisplayText());
+                break;
+            }
+        }
     }
 }
